071050f4b864321baac4d35e5fb2cd512daa1461
name <email>
2023-04-18 08:47:16 +0700

    telebot

---
+++ settings.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const {
+   indonesia
+} = require("./language");
+
+// Website Api (jgn di ganti biar gk eror)
+global.APIs = {
+   alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
+}
+
+//buy apikey premium 0887435047326
+// Free apikey (silahkan login terus ganti Your Key dgn apikey lu)
+global.APIKeys = {
+   'https://api.zeeoneofc.my.id': 'Your Key', // 👉 login https://api.zeeoneofc.my.id to get apikey
+}
+
+//language 
+global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+
+global.BOT_TOKEN = "5826942012:AAF1lANC7x3iLJPntBX-_56YlB2zGzgkQQk" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "Latina - Mdོོ" //your bot name
+global.OWNER_NAME = "ᴹᴿ᭄ ༄ᶜZull - Modz☆࿐ ×፝֟͜×" //your name
+global.OWNER_NUMBER = "6289603792741" //your telegram number
+global.OWNER = ["https://t.me/CyberBadDeath", "https://t.me/TopupAllGame"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
+global.lang = language //don't change
